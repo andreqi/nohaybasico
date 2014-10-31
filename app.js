0ab77cc1999e8461c5851b7ca72ee4d53f9907a4
andreqi <drol.005@gmail.com>
2014-10-30 19:21:50 -0500

    restaurants as json

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var express = require('express'); 
 var app = express(); 
+var fs = require('fs');
 
 var port = 4321;
 app.listen(port);
 
 app.get('/', function(req, res) {
-  res.render('main', {restaurants: [{name: 'Marinsa'}, {name: 'Tio Bigote'}]});
+  get_restaurants(function(data) {
+    res.render('main', {restaurants: data});
+  });
 });
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+function get_restaurants(callback) {
+  fs.readdir('./restaurants', function(err, files) {
+    files = files.filter(function(name) {return name[0] != '.'});
+    var payback = files.map(function(data) {
+      var strFile = fs.readFileSync('./restaurants/' + data, 'utf8');
+      return JSON.parse(strFile); 
+    });
+    callback(payback);
+  });
+}
